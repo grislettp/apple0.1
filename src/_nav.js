fc3f4407a7b6b88e6104e38faeec1a000a0d7268
grislett <grislettgonzalez@gmail.com>
2024-11-10 16:34:38 -0400

    modulos

--- src/_nav.js
+++ src/_nav.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
 import {
-  cilBell,
-  cilCalculator,
-  cilChartPie,
-  cilCursor,
-  cilDescription,
-  cilDrop,
-  cilNotes,
-  cilPencil,
-  cilPuzzle,
-  cilSpeedometer,
-  cilStar,
+  
+  cilSpeedometer, 
+  cilPeople,
+  cilHeart,
+  cilCalendar,
+  cilHome,
+  cilUser,
+  cilGraph,
+  cilCart,
+  cilMobile,
+  cilDollar,
+  cilSitemap
 } from '@coreui/icons'
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
 
 const _nav = [
   {
@@ -21,290 +23,141 @@ const _nav = [
     name: 'Dashboard',
     to: '/dashboard',
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
+    
   },
+
+
+  
   {
-    component: CNavTitle,
-    name: 'Theme',
-  },
-  {
-    component: CNavItem,
-    name: 'Colors',
-    to: '/theme/colors',
-    icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Typography',
-    to: '/theme/typography',
-    icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
-    name: 'Components',
-  },
-  {
+    //Usuario
     component: CNavGroup,
-    name: 'Base',
-    to: '/base',
-    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    name: 'Users', //Usuario 
+    to: '/Users',
+    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
-        name: 'Accordion',
-        to: '/base/accordion',
-      },
+        name: 'Employees',
+        to: '/Users/Employees',
+      }, 
       {
         component: CNavItem,
-        name: 'Breadcrumb',
-        to: '/base/breadcrumbs',
-      },
-      {
-        component: CNavItem,
-        name: 'Cards',
-        to: '/base/cards',
-      },
-      {
-        component: CNavItem,
-        name: 'Carousel',
-        to: '/base/carousels',
-      },
-      {
-        component: CNavItem,
-        name: 'Collapse',
-        to: '/base/collapses',
-      },
-      {
-        component: CNavItem,
-        name: 'List group',
-        to: '/base/list-groups',
-      },
-      {
-        component: CNavItem,
-        name: 'Navs & Tabs',
-        to: '/base/navs',
-      },
-      {
-        component: CNavItem,
-        name: 'Pagination',
-        to: '/base/paginations',
-      },
-      {
-        component: CNavItem,
-        name: 'Placeholders',
-        to: '/base/placeholders',
-      },
-      {
-        component: CNavItem,
-        name: 'Popovers',
-        to: '/base/popovers',
-      },
-      {
-        component: CNavItem,
-        name: 'Progress',
-        to: '/base/progress',
-      },
-      {
-        component: CNavItem,
-        name: 'Spinners',
-        to: '/base/spinners',
-      },
-      {
-        component: CNavItem,
-        name: 'Tables',
-        to: '/base/tables',
-      },
-      {
-        component: CNavItem,
-        name: 'Tabs',
-        to: '/base/tabs',
-      },
-      {
-        component: CNavItem,
-        name: 'Tooltips',
-        to: '/base/tooltips',
-      },
+        name: 'Customers',
+        to: '/Users/Customers',
+      }, 
+
     ],
   },
+
   {
+    //Ventas y ordenes
     component: CNavGroup,
-    name: 'Buttons',
-    to: '/buttons',
-    icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
+    name: 'Sales & Orders', //Sales & Orders 
+    to: '/Sales & Orders',
+    icon: <CIcon icon={cilCart} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
-        name: 'Buttons',
-        to: '/buttons/buttons',
-      },
+        name: 'Sales',
+        to: '/Sales & Orders/Sales',
+      }, 
       {
         component: CNavItem,
-        name: 'Buttons groups',
-        to: '/buttons/button-groups',
-      },
-      {
-        component: CNavItem,
-        name: 'Dropdowns',
-        to: '/buttons/dropdowns',
-      },
+        name: 'Orders',
+        to: '/Sales & Orders/Orders',
+      }, 
+   
     ],
   },
+
   {
+   
+
+    //Reportes y análisis
     component: CNavGroup,
-    name: 'Forms',
-    icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
+    name: 'Reports & Analytics', 
+    to: '/Reports & Analytics',
+    icon: <CIcon icon={cilGraph} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
-        name: 'Form Control',
-        to: '/forms/form-control',
-      },
+        name: 'Report Customers',
+        to: '/Reports & Analytics/Customers_Report',
+      }, 
       {
         component: CNavItem,
-        name: 'Select',
-        to: '/forms/select',
-      },
-      {
-        component: CNavItem,
-        name: 'Checks & Radios',
-        to: '/forms/checks-radios',
-      },
-      {
-        component: CNavItem,
-        name: 'Range',
-        to: '/forms/range',
-      },
-      {
-        component: CNavItem,
-        name: 'Input Group',
-        to: '/forms/input-group',
-      },
-      {
-        component: CNavItem,
-        name: 'Floating Labels',
-        to: '/forms/floating-labels',
-      },
-      {
-        component: CNavItem,
-        name: 'Layout',
-        to: '/forms/layout',
-      },
-      {
-        component: CNavItem,
-        name: 'Validation',
-        to: '/forms/validation',
-      },
+        name: 'Report Sales',
+        to: '/Reports & Analytics/Sales-report',
+      }, 
+   
     ],
   },
+
+
+
+
+  
   {
-    component: CNavItem,
-    name: 'Charts',
-    to: '/charts',
-    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
-  },
-  {
+    //Productos
     component: CNavGroup,
-    name: 'Icons',
-    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
+    name: 'Products', //Productos 
+    to: '/Products',
+    icon: <CIcon icon={cilMobile} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
-        name: 'CoreUI Free',
-        to: '/icons/coreui-icons',
-        badge: {
-          color: 'success',
-          text: 'NEW',
-        },
-      },
-      {
-        component: CNavItem,
-        name: 'CoreUI Flags',
-        to: '/icons/flags',
-      },
-      {
-        component: CNavItem,
-        name: 'CoreUI Brands',
-        to: '/icons/brands',
-      },
+        name: 'List',
+        to: '/Products/List',
+      }, 
+   
     ],
   },
+
+    
+
+
   {
+   
+
+    //Proveedores
     component: CNavGroup,
-    name: 'Notifications',
-    icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
+    name: 'Suppliers', //Proveedores 
+    to: '/Suppliers',
+    icon: <CIcon icon={cilSitemap} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
-        name: 'Alerts',
-        to: '/notifications/alerts',
-      },
-      {
-        component: CNavItem,
-        name: 'Badges',
-        to: '/notifications/badges',
-      },
-      {
-        component: CNavItem,
-        name: 'Modal',
-        to: '/notifications/modals',
-      },
-      {
-        component: CNavItem,
-        name: 'Toasts',
-        to: '/notifications/toasts',
-      },
+        name: 'List',
+        to: '/Suppliers/List',
+      }, 
+   
     ],
   },
+
+
   {
-    component: CNavItem,
-    name: 'Widgets',
-    to: '/widgets',
-    icon: <CIcon icon={cilCalculator} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
-  },
-  {
-    component: CNavTitle,
-    name: 'Extras',
-  },
-  {
+   
+
+    //Financias
     component: CNavGroup,
-    name: 'Pages',
-    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
+    name: 'Financials', //Financias 
+    to: '/Financials',
+    icon: <CIcon icon={cilDollar} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
-        name: 'Login',
-        to: '/login',
-      },
-      {
-        component: CNavItem,
-        name: 'Register',
-        to: '/register',
-      },
-      {
-        component: CNavItem,
-        name: 'Error 404',
-        to: '/404',
-      },
-      {
-        component: CNavItem,
-        name: 'Error 500',
-        to: '/500',
-      },
+        name: 'Invoices',
+        to: '/Financials/Invoices',
+      }, 
+   
     ],
   },
-  {
-    component: CNavItem,
-    name: 'Docs',
-    href: 'https://coreui.io/react/docs/templates/installation/',
-    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
-  },
+
+
+
+
+
+
 ]
 
 export default _nav
---
+++ src/views/financials/invoices.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CForm,
+  CFormInput,
+  CFormLabel,
+  CFormTextarea,
+  CRow,
+  CFormSelect,
+} from '@coreui/react'
+import { DocsExample } from 'src/components'
+
+const ListInvoices = () => {
+
+    return (
+        <CRow>
+          <CCol xs={12}>
+            <CCard className="mb-4">
+              <CCardHeader>
+                <strong>Add Treatment</strong>
+              </CCardHeader>
+              <CCardBody>
+                <DocsExample href="forms/form-control">
+                  <CForm>
+                    {/* Treatment description */}
+                    <CRow className="mb-3">
+                      <CFormLabel htmlFor="idCard" className="col-sm-2 col-form-label">
+                      Description
+                      </CFormLabel>
+                      <div className="col-sm-10">
+                        <CFormTextarea id="idCard" placeholder="Enter the treatment description " />
+                      </div>
+                    </CRow>
+    
+                    {/* Observation */}
+                     <CRow className="mb-3">
+                      <CFormLabel htmlFor="idCard" className="col-sm-2 col-form-label">
+                      Observation
+                      </CFormLabel>
+                      <div className="col-sm-10">
+                        <CFormInput type="tex" id="idCard" placeholder="Enter the observation" />
+                      </div>
+                    </CRow>
+    
+    
+                   {/* Start date */}
+                    <CRow className="mb-3">
+                      <CFormLabel htmlFor="Start date" className="col-sm-2 col-form-label">
+                      Start date 
+                      </CFormLabel>
+                      <div className="col-sm-10">
+                        <CFormInput type="date" id="Start date" />
+                      </div>
+                    </CRow>
+    
+                          {/* End date */}
+                          <CRow className="mb-3">
+                      <CFormLabel htmlFor="End date" className="col-sm-2 col-form-label">
+                      End date
+                      </CFormLabel>
+                      <div className="col-sm-10">
+                        <CFormInput type="date" id="End date" />
+                      </div>
+                    </CRow>
+                    
+                   
+    
+                   {/* Submit Button */}
+                    <CRow className="mb-3">
+                      <div className="col-sm-10 offset-sm-2">
+                        <CButton color="primary" type="submit">Submit</CButton>
+                      </div>
+                    </CRow>
+                  </CForm>
+                </DocsExample>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      )
+}
+
+export default ListInvoices
